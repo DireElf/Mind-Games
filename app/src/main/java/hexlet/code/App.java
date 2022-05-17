4be79acdf586dc02dd38c5014244eab0655d3d1b
DireElf <3672@mail.ru>
2022-05-17 15:51:11 +0700

    add user choice

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,16 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        var input = scanner.nextInt();
+        if (input == 1) {
+            Greetings.greeting();
+        } else if (input == 0) {
+            return;
+        }
     }
 }
-
-
