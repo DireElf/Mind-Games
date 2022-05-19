eda2f9da03f5efc1f25c6a82f2b95e5bb71d2298
DireElf <3672@mail.ru>
2022-05-19 14:59:19 +0700

    new architecture

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,21 +1,8 @@
 package hexlet.code;
-
-import java.util.Scanner;
-
 class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
-        int input = scanner.nextInt();
-        if(input == 0) return;
-        User user = new User();
-        user.setChoice(input);
-        if (input == 1) {
-            user.greeting();
-        } else if (input == 2) {
-            user.greeting();
-            Even newGame = new Even(user);
-            newGame.playEvenGame();
-        }
+        Engine engine = new Engine();
+        engine.chooseGame();
+        engine.startChosenGame();
     }
 }
---
+++ app/src/main/java/hexlet/code/Game.java
@@ -0,0 +1,50 @@
+package hexlet.code;
+
+
+import java.util.Scanner;
+
+public abstract class Game {
+    public String question;
+    public String rightAnswer;
+    public String userAnswer;
+    public int gameCount;
+
+    public void letsPlay(User user) {
+        printHowTo();
+        setQuestion();
+        printQuestion();
+        getRightAnswer();
+        getUserAnswer();
+        while (gameCount < 2) {
+            if (isUserAnswerCorrect()) {
+                System.out.println("Correct!");
+                gameCount++;
+                setQuestion();
+                getRightAnswer();
+                printQuestion();
+                getUserAnswer();
+            } else {
+                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was "
+                        + rightAnswer + ".\nLet's try again, " + user.getName() + "!");
+                return;
+            }
+        }
+        user.congrats();
+    }
+
+    public abstract void printHowTo();
+
+    public abstract void setQuestion();
+
+    public void printQuestion() {
+        System.out.print("Question: " + question + "\nYour answer: ");
+    }
+
+    public void getUserAnswer() {
+        userAnswer = new Scanner(System.in).next();
+    }
+
+    public abstract void getRightAnswer();
+
+    public abstract boolean isUserAnswerCorrect();
+}
