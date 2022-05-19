a03d32e97f320528d8e58e6d0c5d3e0c96dc1ca0
DireElf <3672@mail.ru>
2022-05-19 15:00:27 +0700

    Revert "new architecture"
    This reverts commit eda2f9da03f5efc1f25c6a82f2b95e5bb71d2298.

--- app/src/main/java/hexlet/code/User.java
+++ app/src/main/java/hexlet/code/User.java
@@ -4,12 +4,24 @@ import java.util.Scanner;
 
 public class User {
     private String name;
+    private int choice;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getChoice() {
+        return choice;
+    }
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
     public void greeting() {
         System.out.println("Welcome to the Brain Games");
         Scanner scanner = new Scanner(System.in);
@@ -17,8 +29,5 @@ public class User {
         String name = scanner.nextLine();
         this.setName(name);
         System.out.println("Hello, " + name + "!");
-    }
-    public void congrats() {
-        System.out.println("Congratulations, " + name + "!");
     }
 }
