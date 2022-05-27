0b81acc18afeb02cb67ac38a9fe386a492e3ba3d
DireElf <3672@mail.ru>
2022-05-27 21:21:26 +0700

    fix exclamation

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -96,7 +96,7 @@ public class Engine {
     }
 
     public static void greeting() {
-        System.out.println("Welcome to the Brain Games");
+        System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
         userName = scanner.nextLine();
