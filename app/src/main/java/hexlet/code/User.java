60c32ee7b1292f85a1dbe371d3c61a41ed54b4a0
DireElf <3672@mail.ru>
2022-05-29 10:24:50 +0700

    excl

--- app/src/main/java/hexlet/code/User.java
+++ app/src/main/java/hexlet/code/User.java
@@ -10,7 +10,7 @@ public class User {
     }
 
     public static void greeting() {
-        System.out.println("Welcome to the Brain Games");
+        System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
         name = scanner.nextLine();
