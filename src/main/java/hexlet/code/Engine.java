44115b5648b0ebad323407467a395fb824a8bcd2
DireElf <3672@mail.ru>
2022-05-31 09:48:48 +0700

    remove useless variables

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -10,15 +10,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Engine {
-    private static final int EVEN_NUMBER = 2;
-    private static final int CALC_NUMBER = 3;
-    private static final int GCD_NUMBER = 4;
-    private static final int PROGRESSION_NUMBER = 5;
-    private static final int PRIME_NUMBER = 6;
-    private static final int EXIT_NUMBER = 0;
     public static final int ROUNDS_NUMBER = 3;
 
-    private static int userChoice;
+    private static String userChoice;
     private static int gameCount;
     private static String question;
     private static String[] tasks;
@@ -28,38 +22,38 @@ public class Engine {
     public static void chooseGame() {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
-        userChoice = new Scanner(System.in).nextInt();
+        userChoice = new Scanner(System.in).next();
         System.out.println("Your choice: " + userChoice);
     }
     public static void startChosenGame() {
-        if (userChoice == EXIT_NUMBER) {
+        if (userChoice.equals("0")) {
             return;
         }
         switch (userChoice) {
             default:
                 Cli.greeting();
                 break;
-            case EVEN_NUMBER:
+            case "2":
                 initialise(Even.QUESTION, Even.defineTasks());
                 initialise(Even.defineRightAnswers(tasks));
                 play();
                 break;
-            case CALC_NUMBER:
+            case "3":
                 initialise(Calc.QUESTION, Calc.defineTasks());
                 initialise(Calc.defineRightAnswers(tasks));
                 play();
                 break;
-            case GCD_NUMBER:
+            case "4":
                 initialise(GCD.QUESTION, GCD.defineTasks());
                 initialise(GCD.defineRightAnswers(tasks));
                 play();
                 break;
-            case PROGRESSION_NUMBER:
+            case "5":
                 initialise(Progression.QUESTION, Progression.defineTasks());
                 initialise(Progression.defineRightAnswers());
                 play();
                 break;
-            case PRIME_NUMBER:
+            case "6":
                 initialise(Prime.QUESTION, Prime.defineTasks());
                 initialise(Prime.defineRightAnswers(tasks));
                 play();
